5bc11835077d2d1fff0f741a8c2dbb299509b9d8
Sihle Malambe <malambesihle1@gmail.com>
2025-12-11 01:37:19 +0200

    Update NBASEASONS.R

--- NBASEASONS.R
+++ NBASEASONS.R
@@ -24,16 +24,16 @@ ggplot(career_stats, aes(x = CareerPTS, y = CareerTOV)) +
         axis.text = element_text(color = "white"),
         axis.title = element_text(color = "white"),
         plot.title = element_text(color = "white")) +
-  labs(title = "PTS vs TOV", x = "Points Scored", y = "Turnovers Committed")
+  labs(title = "Career Points vs Career Turnovers", x = "Careers Points", y = "Career Turnovers Committed")
 
 ##Top Scorer per Year
-topscorers <- NBA %>%
+top_scorers <- NBA %>%
   filter(!is.na(PTS)) %>%
   group_by(Year) %>%
   slice_max(PTS, n = 1) %>%
   select(Year, Player, PTS)
 cat("====TOP SCORERS PER SEASON====")
-print(topscorers, n = nrow(topscorers))
+print(top_scorers, n = nrow(top_scorers))
 
 ##Top Turnovers per Year
 topturnovers <- NBA %>%
@@ -54,10 +54,34 @@ cat("====TOP ASSISTS PER SEASON====")
 print(topassists, n = nrow(topassists))
 
 AveAge <- mean(NBA$Age[NBA$PTS > 30], na.rm = TRUE)
-PtsToAst <- cor(NBA$PTS,NBA$AST, use = "complete.obs")
-PtsToStls <- cor(NBA$PTS, NBA$STL, use = "complete.obs")
-PtsToTRB <- cor(NBA$PTS, NBA$TRB., use = "complete.obs")
 
-cat("Correlation of Points to Assists",PtsToAst)
-cat("Correlation of Points to Steals",PtsToStls)
 cat("Average Age of NBA Players",round(AveAge,))
+
+##Correlation Matrix
+cor_Matrix <- NBA %>% 
+  select(PTS, AST, TOV, STL, TRB) %>%
+  cor(use = "complete.obs")
+cat("====Correlation Matrix====")
+print(cor_Matrix)
+
+##Era Comparison
+NBA <- NBA %>%
+  mutate(Era = case_when(
+    Year <= 1969 ~ "50s-60s",
+    Year >= 1970 & Year <= 1979 ~ "70s",
+    Year >= 1980 & Year <= 1989 ~ "80s",
+    Year >= 1990 & Year <= 1999 ~ "90s",
+    Year >= 2000 & Year <= 2009 ~ "2000s",
+    Year >= 2010 & Year <= 2019 ~ "2010s"
+  )
+         )
+
+##Summarise Stats by Era
+Era_Stats <- NBA %>% 
+  group_by(Era) %>%
+  summarise(
+    AvgPTS = mean(PTS, na.rm = TRUE),
+    AvgAST = mean(AST, na.rm = TRUE)
+  )
+
+print(Era_Stats)
